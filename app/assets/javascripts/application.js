c77aad11d0a2b4e61e733a774d2eb25c36d48c31
Nathan How <nathan.how@live.ca>
2015-11-15 02:04:33 -0500

    Javascript Update
    Changes to all functions, inclusion of global variables, save page
    button,

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,99 @@
 //= require bootstrap-datepicker
 //= require turbolinks
 //= require_tree .
+var developmentChanges = [];
+var vaccineChanges = [];
+var allergyChanges = [];
+var birthdate = new Date("03/25/1995");
+
+function editFunc(element,devnum){
+	element.parentElement.innerHTML = '<span class="input-group"> \
+											<input type="text" size="10" class="input-sm form-control pull-left" value="mm/dd/yyyy" /> \
+											<span class="input-group-addon"> \
+												<span id="' + devnum + '" class="fa fa-floppy-o" onclick="saveFunc(this,this.id)"></span> \
+											</span> \
+										</span>';
+	}
+function saveFunc(element,devnum){
+	var eventDate = new Date(String(element.parentElement.previousElementSibling.value));
+	var eventAge = (eventDate-birthdate)/2629929600;
+	element.parentElement.parentElement.parentElement.previousElementSibling.innerHTML = Math.round(eventAge) + " Months"
+	element.parentElement.parentElement.parentElement.innerHTML = eventDate.toDateString().slice(4) + '<i id="' + devnum + '" \
+																class="fa fa-pencil pull-right" onclick="editFunc(this,this.id)"></i>';															
+	developmentChanges.push([devnum,eventDate]);
+	}
+	
+function addVacc(element){
+	element.parentElement.parentElement.innerHTML = '<td> \
+														<span class="input-group"> \
+															<input id="nameElement" type="text" size="10" class="input-sm form-control pull-left" value="Vaccine Name" /> \
+														</span> \
+													</td> \
+													<td> Let us handle the math </td> \
+													<td> \
+														<span class="input-group"> \
+															<input type="text" size="10" class="input-sm form-control pull-left" value="mm/dd/yyyy" /> \
+															<span class="input-group-addon"> \
+																<span class="fa fa-floppy-o" onclick="saveVacc(this,' + "'nameElement'" + ')"></span> \
+															</span> \
+														</span> \
+													</td>';
+	}
+	
+function saveVacc(element,nameID){
+	var vaccName = String(document.getElementById(nameID).value);
+	var vaccDate = new Date(element.parentElement.previousElementSibling.value);
+	var vaccAge = (vaccDate - birthdate)/2629929600;
+	element.parentElement.parentElement.parentElement.parentElement.innerHTML = '<td>' + vaccName + '</td> \
+																				<td>' + Math.round(vaccAge) + ' Months </td> \
+																				<td>' + vaccDate.toDateString().slice(4) + '<i class="fa fa-pencil pull-right" onclick="editFunc(this,this.id)"></i></td>';
+	vaccineChanges.push([vaccName,vaccDate]);
+   }
+	
+function addAllergy(element){
+	element.parentElement.parentElement.innerHTML = '<td> \
+														<span class="input-group"> \
+															<input id="allergenElement" type="text" size="10" class="input-sm form-control pull-left" value="Allergen" /> \
+														</span> \
+													</td> \
+													<td> \
+														<span class="input-group"> \
+															<label for="sel1">Select Severity:</label> \
+															  <select class="form-control" id="sel1"> \
+																<option>Mild</option> \
+																<option>Moderate</option> \
+																<option>Severe</option> \
+																<option>Unknown</option> \
+															  </select> \
+														</span> \
+													<td> \
+														<span class="input-group"> \
+															<input type="text" size="10" class="input-sm form-control pull-left" value="mm/dd/yyyy" /> \
+															<span class="input-group-addon"> \
+																<span class="fa fa-floppy-o" onclick="saveAllergy(this,' + "'allergenElement','sel1'" + ')"></span> \
+															</span> \
+														</span> \
+													</td>';
+	}
+
+function saveAllergy(element,allergenID,severityID){
+	var allergyName = String(document.getElementById(allergenID).value);
+	var allergySeverity = document.getElementById(severityID).selectedIndex;
+	var allergyDate = new Date(element.parentElement.previousElementSibling.value);
+	if (allergySeverity == 0){
+		allergySeverity = "Mild";
+	} else if (allergySeverity == 1) {
+		allergySeverity = "Moderate";
+	} else if (allergySeverity == 2) {
+		allergySeverity = "Severe";
+	} else {
+		allergySeverity = "Unknown";
+	};
+	element.parentElement.parentElement.parentElement.parentElement.innerHTML = '<td>' + allergyName + '</td> \
+																				<td>' + allergySeverity + '</td> \
+																				<td>' + allergyDate.toDateString().slice(4) + '<i class="fa fa-pencil pull-right" onclick="editFunc(this,this.id)"></i></td>';
+	allergyChanges.push([allergyName,allergySeverity,allergyDate]);
+   }
+  
+function savePage(){
+}
