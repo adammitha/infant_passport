9fed938ef7204e46d8aa17866e17f4dd1db3b8f5
Nathan How <nathan.how@live.ca>
2016-01-09 17:07:10 -0500

    Tests and Allergies

--- test/models/timeline_test.rb
+++ test/models/timeline_test.rb
@@ -1,19 +1,5 @@
 require 'test_helper'
 
 class TimelineTest < ActiveSupport::TestCase
-	def setup
-		@parent = parents(:adam)
-		@child = @parent.children.build(first_name: "Luke", last_name: "Skywalker", date_of_birth: DateTime.parse("1995-01-01"), gender: false)
-		@timeline = @child.timeline.build()
-	end
-	
-	test "should be valid" do
-		assert @timeline.valid?
-	end
-	
-	test "child id should be present" do
-		@timeline.child_id = nil
-		assert_not @timeline.valid?
-	end
-	
+
 end
