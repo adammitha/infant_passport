c24e230389ef3208995884c72395aa4d35ffd82f
Nathan How <nathan.how@live.ca>
2016-04-15 17:56:02 -0400

    Height additions and deletions created

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,7 +1,49 @@
 class ChartsController < ApplicationController
   def show
+    @chart = Chart.find(params[:id])
+    @heights = @chart.heights
+    @weights = @chart.weights
   end
 
   def update
+    data = JSON.parse params[:formData]
+	updateChart(Chart.find(params[:id]),data)
+    redirect_to Chart.find(params[:id])  
   end
+  
+    private
+    def updateChart(chart,data)
+      additions = data['additions']
+      deletions = data['deletions']
+      addHeights(additions['height'],chart)
+      addWeights(additions['weight'],chart)
+      deleteHeights(deletions['height'],chart)
+      deleteWeights(deletions['weight'],chart)
+    end
+
+    def addHeights(heights,chart)
+      heights.each do |height|
+        height = chart.heights.build(value:height[0],date:height[1].to_datetime)
+        height.save
+      end
+    end
+
+    def addWeights(weights,chart)
+      weights.each do |weight|
+        weight = chart.weights.build(value:weight[0],date:weight[1].to_datetime)
+        weight.save
+      end
+    end
+
+    def deleteHeights(heights,chart)
+      heights.each do |height|
+        Height.find(height.to_i).destroy
+      end
+    end
+
+    def deleteWeights(weights,chart)
+      weights.each do |weight|
+        Weight.find(weight.to_i).destroy
+      end
+    end
 end
--- app/models/chart.rb
+++ app/models/chart.rb
@@ -1,5 +1,5 @@
 class Chart < ActiveRecord::Base
   belongs_to :child
-  has_many :height, dependent: :destroy
-  has_many :weight, dependent: :destroy
+  has_many :heights, dependent: :destroy
+  has_many :weights, dependent: :destroy
 end
